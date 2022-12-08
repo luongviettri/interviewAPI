4b017430171354f69c66519e41865f31e87bed29
Tri Luong <viettriluong@gmail.com>
2022-12-08 10:20:25 +0700

    ok

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ dotenv.config({
 
 const app = require('./app');
 
-app.use(cors()); //! test
+const corsOptions = {
+  origin: 'http://localhost:4000',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions)); //! test
 
 const DB = process.env.DATABASE.replace(
   '<PASSWORD>',
