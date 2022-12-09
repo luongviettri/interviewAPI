aa39a57ef583562e8898c6fd1370c8be7faaec32
Tri Luong <viettriluong@gmail.com>
2022-12-09 09:43:34 +0700

    ok

--- routes/viewRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const viewController = require('../controllers/viewController');
-
-const router = express.Router();
-
-router.get('/', viewController.getOverview);
-router.get('/tour/:slug', viewController.getTour);
-module.exports = router;
